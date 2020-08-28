7e7fec6a3c00ae4c001cf75832cd5952a5308cd3
Alejandro <alejocano22@gmail.com>
2020-08-28 11:36:09 -0500

    Members animation

--- main.js
+++ main.js
@@ -3,21 +3,22 @@ const burguerButton = document.querySelector('.burguer-button');
 const mobileNav = document.querySelector('.mobile-nav');
 const memberBar = document.querySelectorAll('.member-bar');
 const memberBox = document.querySelectorAll('.member-box');
+const workBox = document.querySelectorAll('.work-modal');
+const workModal = document.querySelectorAll('.work-modal');
 
+// Array of welcome images
 const welcomeImages = [];
 let imageIndex = 0;
 welcomeImages[0] = 'url("./images/home-bg.jpg") center/cover no-repeat border-box';
 welcomeImages[1] = 'url("./images/home-bg-2.jpg") center/cover no-repeat border-box';
 
+// To change welcome image
 function changeWelcomeImage() {
   imageIndex = (imageIndex === (welcomeImages.length - 1)) ? 0 : imageIndex += 1;
   welcomeImage.style.background = welcomeImages[imageIndex];
 }
 
-// Change welcome image
-setInterval(changeWelcomeImage, 4000);
-
-// mobile nav (Open - Close)
+// To open/close mobile nav bar
 burguerButton.addEventListener('click', () => {
   if (mobileNav.classList.contains('mobile-nav-open')) {
     mobileNav.classList.remove('mobile-nav-open');
@@ -26,17 +27,35 @@ burguerButton.addEventListener('click', () => {
   }
 });
 
+// To display member bar
 function handleMemberBar() {
   for (let i = 0; i < memberBox.length; i += 1) {
     memberBox[i].addEventListener('mouseenter', () => {
       memberBar[i].classList.add('member-bar-open');
-      console.log('in');
     });
     memberBox[i].addEventListener('mouseleave', () => {
       memberBar[i].classList.remove('member-bar-open');
-      console.log('out');
     });
   }
 }
 
+// To display work modal
+function handleWorkModal() {
+  for (let i = 0; i < workBox.length; i += 1) {
+    workBox[i].addEventListener('mouseenter', () => {
+      workModal[i].classList.add('work-modal-open');
+    });
+    workBox[i].addEventListener('mouseleave', () => {
+      workModal[i].classList.remove('work-modal-open');
+    });
+  }
+}
+
+// Call change welcome image
+setInterval(changeWelcomeImage, 4000);
+
+// Call display member bar
 handleMemberBar();
+
+// Call display work modal
+handleWorkModal();
